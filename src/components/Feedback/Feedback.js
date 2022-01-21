2663bb98711e27c82dbbcd28805abc41e1659148
Oleh Semennykov <sumcue@gmail.com>
2022-01-22 00:29:25 +0200

    add css module

--- src/components/Feedback/Feedback.js
+++ src/components/Feedback/Feedback.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Fragment } from 'react/cjs/react.production.min';
-import './Feedback.css';
+import styles from './Feedback.module.css';
 
 class Feedback extends React.Component {
   //  constructor () {
@@ -22,39 +22,63 @@ class Feedback extends React.Component {
   handleGoodButton = () => {
     console.log(' good');
     console.log(this);
-    this.setState({
-      good: 13,
+    this.setState(prevState => {
+      return {
+        good: prevState.good + 1,
+      };
     });
   };
   handleNeutralButton = () => {
     console.log('neutral');
     console.log(this);
+    this.setState(prevState => {
+      return {
+        neutral: prevState.neutral + 1,
+      };
+    });
   };
   handleBadButton = () => {
     console.log('bad');
     console.log(this);
+    this.setState(prevState => {
+      return {
+        bad: prevState.bad + 1,
+      };
+    });
   };
 
   render() {
     return (
       <Fragment>
-        <h1>Please leave feedback</h1>
-        <div>
-          <button type="button" onClick={this.handleGoodButton}>
+        <h1 className={styles.Feedback__header}>Please leave feedback</h1>
+        <div className="Feedback__container">
+          <button
+            type="button"
+            onClick={this.handleGoodButton}
+            className="Feedback__btn"
+          >
             good
           </button>
-          <button type="button" onClick={this.handleNeutralButton}>
+          <button
+            type="button"
+            onClick={this.handleNeutralButton}
+            className="Feedback__btn"
+          >
             neutral
           </button>
-          <button type="button" onClick={this.handleBadButton}>
+          <button
+            type="button"
+            onClick={this.handleBadButton}
+            className="Feedback__btn"
+          >
             bad
           </button>
         </div>
-        <h2>Statistics</h2>
-        <ul>
-          <li>Good : {this.state.good}</li>
-          <li>Neutral : {this.state.neutral}</li>
-          <li>Bad : {this.state.bad}</li>
+        <h2 className="Feedback__title">Statistics</h2>
+        <ul className="Feedback__list">
+          <li className="Feedback__item">Good : {this.state.good}</li>
+          <li className="Feedback__item">Neutral : {this.state.neutral}</li>
+          <li className="Feedback__item">Bad : {this.state.bad}</li>
         </ul>
       </Fragment>
     );
